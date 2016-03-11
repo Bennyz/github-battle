8a11aa5748b66309b0e48a84cca73259aa3ef672
Benny Zlotnik <uber442@gmail.com>
2016-03-11 21:09:19 +0200

    First routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,7 +9,9 @@ var Home = require('../components/Home');
 var routes = (
   <Router>
     <Route path='/' component={Main}>
-      
+      <Route path='/home' component={Home} />  
     </Route>
   </Router>
 );
+
+module.exports = routes;
