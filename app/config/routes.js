6558a77202b1a7db5db03c40379503966df9df93
Benny Zlotnik <uber442@gmail.com>
2016-03-12 00:27:54 +0200

    Fix up the routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,18 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <Route path='/home' component={Home} />  
+      <IndexRoute component={Home} />  
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleCOntainer} />
+      <Route path='results' component={ResultsContainer} />
     </Route>
   </Router>
 );
