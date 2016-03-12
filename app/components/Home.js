003c40762b759ae6fd504dc119d4cd689f7c91ba
Benny Zlotnik <uber442@gmail.com>
2016-03-12 13:33:10 +0200

    Reshape home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,18 @@
-var  React = require('react');
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReatRouter = require('react-router');
+var Link = ReatRouter.Link;
 
 var Home = React.createClass({
   render: function() {
     return (
-      <div className="jumbotron">Hello from home</div>
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>Github Battle</h1>
+        <p className='lead'></p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+        </Link>
+      </div>
     )
   }
 });
